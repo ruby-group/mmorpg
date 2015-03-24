261b204cfe5e9728bf11bebf98ca8b4d7e400291
Alan Lobo <alansaviolobo@gmail.com>
2015-03-24 11:52:07 +0530

    removed the usage of the context module and captured settings in a custom page.

--- docroot/sites/all/modules/features/mmorpg_siteskins/mmorpg_siteskins.js
+++ docroot/sites/all/modules/features/mmorpg_siteskins/mmorpg_siteskins.js
@@ -2,19 +2,21 @@
  * Created by alan on 23/3/15.
  */
 jQuery(document).ready(function () {
-    var history = JSON.parse(jQuery.cookie('siteskins') || '{}');
-    var key = '_' + Drupal.settings.siteskins.id;
-    if (history[key] == undefined) history[key] = -1;
-    history[key] = (history[key] + 1) % Drupal.settings.siteskins.data.length;
-    jQuery.cookie('siteskins', JSON.stringify(history));
-    var data = Drupal.settings.siteskins.data[history[key]];
-    jQuery('#skin-tracking-area').attr('href', data.link);
-    jQuery('.main').css({
-      "background-color": data.color,
-      "background-image": 'url(' + data.image + ')',
-      "background-repeat": 'no-repeat',
-      "background-attachment": 'scroll',
-      "background-position": 'left top'
-    });
+    if (Drupal.settings.siteskins != undefined) {
+      var key = '_' + Drupal.settings.siteskins.id;
+      var history = JSON.parse(jQuery.cookie('siteskins') || '{}');
+      if (history[key] == undefined) history[key] = -1;
+      history[key] = (history[key] + 1) % Drupal.settings.siteskins.data.length;
+      jQuery.cookie('siteskins', JSON.stringify(history));
+      var data = Drupal.settings.siteskins.data[history[key]];
+      jQuery('#skin-tracking-area').attr('href', data.link);
+      jQuery('.main').css({
+        "background-color": data.color,
+        "background-image": 'url(' + data.image + ')',
+        "background-repeat": 'no-repeat',
+        "background-attachment": 'scroll',
+        "background-position": 'left top'
+      });
+    }
   }
 );
