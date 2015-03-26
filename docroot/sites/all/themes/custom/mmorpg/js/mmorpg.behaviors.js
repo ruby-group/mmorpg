e8f77a3df73bc1d69c669faf8cd7e4742a72aa4b
Kumar Mantesh <kumar.mantesh007@gmail.com>
2015-03-26 18:52:56 +0530

    added promoted signup now link to mmorpg_signup_now feature
    modfied theme related changes for promoted signupnow links

--- docroot/sites/all/themes/custom/mmorpg/js/mmorpg.behaviors.js
+++ docroot/sites/all/themes/custom/mmorpg/js/mmorpg.behaviors.js
@@ -56,25 +56,4 @@
       });
     }
   };
-  Drupal.behaviors.mmorpgGameToDisplayBehavior = {
-    attach: function (context, settings) {
-      var date = new Date();
-      var minute = date.getMinutes();
-      $(".view-games-to-play .view-content .tier").each(function(index,element) {
-        var tierLimits = $(element).find("div .views-field-field-tiers-max-links-to-display .field-content").html();
-        var rows = $(element).find(".views-row");
-        var rowsArray = $.map(rows, function (value, index) {
-          return value;
-        });
-        rotateCount = Math.floor(minute / 5);
-        for (i = 0; i < rotateCount; i++) {
-          var temp = rowsArray.shift();
-          rowsArray.push(temp);
-        };
-        var displayRows = rowsArray.slice(0, tierLimits);
-        $(element).html(displayRows);
-      });
-    }
-  };
-
 })(jQuery);
