dc6e0f69e82aa11bbcc5b561c23d14f2ae12e9b7
Kumar Mantesh <kumar.mantesh007@gmail.com>
2015-04-08 12:51:16 +0530

    mmos block with signup now links

--- docroot/sites/all/modules/features/mmorpg_signup_now/mmorpg_signup_now.js
+++ docroot/sites/all/modules/features/mmorpg_signup_now/mmorpg_signup_now.js
@@ -2,10 +2,12 @@ Drupal.behaviors.mmorpgGameToDisplayBehavior = {
   attach: function (context, settings) {
     var date = new Date();
     var minute = date.getMinutes();
+
+    //hide the promoted Signup now links from tiers
     jQuery(".view-promoted-signup-now-link .view-content .tier").each(function (index, element) {
-      var tierLimits = $(element).find("div .views-field-field-tiers-max-links-to-display .field-content").html();
-      var rows = $(element).find(".views-row");
-      var rowsArray = $.map(rows, function (value, index) {
+      var tierLimit = jQuery(element).find("div .views-field-field-tiers-max-links-to-display .field-content").html();
+      var rows = jQuery(element).find(".views-row");
+      var rowsArray = jQuery.map(rows, function (value, index) {
         return value;
       });
       var rotateCount = Math.floor(minute / 5);
@@ -13,8 +15,25 @@ Drupal.behaviors.mmorpgGameToDisplayBehavior = {
         var temp = rowsArray.shift();
         rowsArray.push(temp);
       }
-      var displayRows = rowsArray.slice(0, tierLimits);
+      var displayRows = rowsArray.slice(0, tierLimit);
       jQuery(element).html(displayRows);
     });
+
+    //calculate no of promoted links
+    var promoCount = jQuery(".view-promoted-signup-now-link .view-content .tier .views-row").length;
+
+    //get the limit set on total mmo block
+    var requiredLimit = Drupal.settings.mmorpg_signup_now.mmo_limit;
+
+    //get count of no of signupnow links to display
+    signupNowLimit = requiredLimit - promoCount;
+
+    //hide the other signup now links
+    if (signupNowLimit > 0) {
+      jQuery(".view-promoted-signup-now-link .view-content > .views-row:gt(" + 
+        (Number(signupNowLimit) - 1) +")").each(function (index, element) {
+          jQuery(this).hide();
+      });
+    }
   }
 };
