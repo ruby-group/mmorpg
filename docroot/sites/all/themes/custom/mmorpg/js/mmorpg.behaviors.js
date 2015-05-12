49da1f3e655aebf031daf902d90eb37c857c4c2b
Aliya Khan <aliya@axelerant.com>
2015-05-12 11:41:54 +0530

    Ratings final tweaks

--- docroot/sites/all/themes/custom/mmorpg/js/mmorpg.behaviors.js
+++ docroot/sites/all/themes/custom/mmorpg/js/mmorpg.behaviors.js
@@ -1,25 +1,37 @@
 (function ($) {
-   Drupal.behaviors.mmorpgGameRatings = {
+  Drupal.behaviors.mmorpgToggleRatings = {
+    jQuery("#rate-view .btn").click(function(event) {
+      jQuery("#rate-view").hide();
+      jQuery(".cast-view").show();
+      event.preventDefault()
+    });
+    jQuery(".cast-view .btn").click(function(event) {
+      jQuery("#rate-view").show();
+      jQuery(".cast-view").hide();
+      event.preventDefault()
+    });
+  }
+  Drupal.behaviors.mmorpgGameRatings = {
     attach: function (context, settings) {
       var userRatingAverage = function() {
        var container = $('.view-id-game_ratings .views-field');
-      $.each(container, function(index, el) {
+       $.each(container, function(index, el) {
         var userVote = parseInt($(this).find('.user-vote').text());
         var avgVote = parseInt($(this).find('.average-rating').text());
         var totalAvg = (userVote + avgVote) / 8;
         if ($(this).find(".total-average").length == 0) {
           var node = "<span class=total-average>" + totalAvg + "</span>";
-          } else {
-            $(this).find(".total-average").text(totalAvg);
-          }
-          $(this).find(".field-content").before(node);
+        } else {
+          $(this).find(".total-average").text(totalAvg);
+        }
+        $(this).find(".field-content").before(node);
         userVote = undefined;
         avgVote = undefined;
         totalAvg = undefined;
       });
-    }
+     }
 
-    var gameAverage = function() {
+     var gameAverage = function() {
       var container = $('.view-display-id-active_ratings .total-average');
       var store = 0;
       $.each(container, function(index, el) {
@@ -32,6 +44,6 @@
 
     userRatingAverage();
     gameAverage();
-    }
-  };
+  }
+};
 })(jQuery);
