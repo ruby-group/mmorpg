483e81285354418e86f1815f1fc7ce94280ccd6c
Kumar Mantesh <kumar.mantesh007@gmail.com>
2015-05-21 13:54:16 +0530

    modified the author display on node

--- docroot/sites/all/themes/custom/mmorpg/template.php
+++ docroot/sites/all/themes/custom/mmorpg/template.php
@@ -5,3 +5,14 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * mmorpg theme.
  */
+
+function mmorpg_preprocess_node(&$vars) {
+  $date = $vars['date'];
+  $dateObj = DateTime::createFromFormat("D, m/d/Y - H:i",$date);
+  $requiredDate = $dateObj->format("F j, Y");
+  $vars['submitted'] = t('Posted by !username on !datetime',
+  array(
+    '!username' => $vars['name'],
+    '!datetime' => $requiredDate,
+  ));
+}
