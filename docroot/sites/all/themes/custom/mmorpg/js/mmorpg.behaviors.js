d9070bd6d8a88e2cdb30ca5f6f2609db472c7751
Aliya Khan <aliya@axelerant.com>
2015-05-29 14:54:14 +0530

    HEader image overlap for sweepstakes review

--- docroot/sites/all/themes/custom/mmorpg/js/mmorpg.behaviors.js
+++ docroot/sites/all/themes/custom/mmorpg/js/mmorpg.behaviors.js
@@ -1,10 +1,16 @@
 (function ($) {
   Drupal.behaviors.sideBlockRate = {
     attach: function (context) {
-      var value = jQuery(".view-promoted-signup-now-link .views-field-average-game-rating .field-content").text();
+      var value = $(".view-promoted-signup-now-link .views-field-average-game-rating .field-content").text();
       value = parseFloat(value);
-      var wrap = jQuery(".view-promoted-signup-now-link .views-field-average-game-rating .field-content");
-      jQuery(wrap).width(value+'%');
+      var wrap = $(".view-promoted-signup-now-link .views-field-average-game-rating .field-content");
+      $(wrap).width(value+'%');
+    }
+  }
+  Drupal.behaviors.overviewoverlap = {
+    attach: function (context) {
+      var afterthis = $(".pane-node-field-hero-image").next("p");
+      $(afterthis).addClass("field-overview");
     }
   }
 })(jQuery);
